19335bba3bcf61d948bb06027912a7c8eefd243e
Richard Quizon <codenomics@proton.me>
2023-06-06 15:19:12 -0700

    new branch with_router: App.jsx changes.

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import './App.css';
+import { useEffect, useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import SearchBar from './Components/SearchBar'
+import Gallery from './Components/Gallery'
+import AlbumView from './Components/AlbumView';
+import ArtistView from './Components/ArtistView';
+
+function App() {
+  let [search, setSearch] = useState("")
+  let [data, setData] = useState([])
+  let [message, setMessage] = useState("Search for music!")
+
+  const API_URL = 'http://itunes.apple.com/search?term='
+
+  useEffect(() => {
+    if (search) {
+       document.title = `${search} Music`
+       fetch(API_URL + search)
+       .then(res => res.json())
+       .then(resData => {
+          if (resData.results.length > 0) {
+            setData(resData.results)
+          } else {
+            setMessage("No results found!")
+          }
+       })
+    }
+  }, [search])
+
+  const handleSearch = (e, term) => {
+    e.preventDefault()
+    setSearch(term)
+  }
+
+  return (
+    <div className="App">
+      <SearchBar handleSearch={handleSearch} />
+      {message}
+      <Gallery data={data} />
+      <AlbumView />
+      <ArtistView />
+    </div>
+  );
+}
+
+export default App;
