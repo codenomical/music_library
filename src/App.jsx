e718d92169235021721be932a388229b17914185
Richard Quizon <codenomics@proton.me>
2023-06-09 20:38:31 -0700

    added DataContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+// Imports for with_context, with_router and with_suspense
 import './App.css';
 import { useEffect, useState, Fragment } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
@@ -5,6 +6,7 @@ import SearchBar from './Components/SearchBar'
 import Gallery from './Components/Gallery'
 import AlbumView from './Components/AlbumView';
 import ArtistView from './Components/ArtistView';
+import { DataContext } from './Context/DataContext'
 
 function App() {
   let [search, setSearch] = useState("")
@@ -36,22 +38,25 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Routes>
-          <Route 
-          path="/" 
-          element={
-            <Fragment>
-              <SearchBar handleSearch={handleSearch} />
-              {message}
-              <Gallery data={data} />
-            </Fragment>
-          } />
-          <Route path="/album/:id" element={<AlbumView />} />
-          <Route path="/artist/:id" element={<ArtistView />} />
-        </Routes>
+        <SearchBar handleSearch={handleSearch} />
+        {message}
+        <DataContext.Provider value={data}>
+          <Routes>
+            <Route 
+            path="/" 
+            element={
+              <Fragment>
+                <Gallery />
+              </Fragment>
+            } /> 
+            <Route path="/album/:id" element={<AlbumView />} />
+            <Route path="/artist/:id" element={<ArtistView />} />
+          </Routes>
+        </DataContext.Provider>  
       </div>
     </Router>  
   )
 }
-
+// with_router assignment added the id. 
+// DataContext added to wrap around the <Gallery /> and Routes.
 export default App;
