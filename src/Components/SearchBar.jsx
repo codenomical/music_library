22b14dbfa961a26c2785000ea46b1e641cc9b580
Richard Quizon <codenomics@proton.me>
2023-06-11 05:53:04 -0700

    Refactored: added useContext and remvoed props

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
+import { useContext } from 'react'
 
-function SearchBar (props) {
-    let [searchTerm, setSearchTerm] = useState("")
+function SearchBar () {
+    const { term, handleSearch } = useContext(SearchContext)
 
     return (
-        <form onSubmit = {(e) => props.handleSearch(e, searchTerm)}>
-            <input type="text" placeholder="Enter a search term here" onChange={(e) => setSearchTerm(e.target.value)} />
-            <input type="submit" />
+        <form>
+            <input ref={term} type="text" placeholder="Search Here" />
+            <button onClick={(e) => handleSearch(e, term)}>Submit</button>
         </form>
     )
 }
